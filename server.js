ff9af119a31ff7880c0e60cd2a363d5f70b66511
Alexander Hanssen <alexhsn1@gmail.com>
2015-08-06 20:18:15 +0200

    Cleaned up boilerplate code

--- server.js
+++ server.js
@@ -28,18 +28,6 @@ app.get('/cards.json', function(req, res) {
     res.json(JSON.parse(data));
   });
 });
-/*
-app.post('/comments.json', function(req, res) {
-  fs.readFile('comments.json', function(err, data) {
-    var comments = JSON.parse(data);
-    comments.push(req.body);
-    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
-      res.setHeader('Cache-Control', 'no-cache');
-      res.json(comments);
-    });
-  });
-});
-*/
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
